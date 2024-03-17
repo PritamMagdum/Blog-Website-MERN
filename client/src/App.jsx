530dbfad495158826ef46a861b01503d55aedb2b
Pritam <pritammagdum24@gmail.com>
2024-03-17 11:29:11 +0530

    Added Flowbite Configuration for UI Designing and for Better User Experience

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,11 +7,13 @@ import Projects from "./pages/Projects";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <div>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
