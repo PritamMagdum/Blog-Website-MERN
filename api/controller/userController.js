03ed0ee17a174d56e7252de6469a43bdf1a26e27
Pritam <pritammagdum24@gmail.com>
2024-03-19 21:37:22 +0530

    Fixed Image Update Issue

--- api/controller/userController.js
+++ api/controller/userController.js
@@ -32,24 +32,23 @@ export const updateUser = async (req, res, next) => {
     if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
       return next(400, "Username can only contain letter and numbers");
     }
-
-    try {
-      const updatedUser = await User.findByIdAndUpdate(
-        req.params.userId,
-        {
-          $set: {
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.password,
-            profilePicture: req.body.profilePicture,
-          },
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          profilePicture: req.body.profilePicture,
         },
-        { new: true }
-      );
-      const { password, ...rest } = updatedUser._doc;
-      res.status(200).json(rest);
-    } catch (error) {
-      next(error);
-    }
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
   }
 };
