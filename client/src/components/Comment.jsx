0e91619e37c70a4cb86d2790f9230c431769ca01
Pritam <pritammagdum24@gmail.com>
2024-03-21 15:39:38 +0530

    Added Functionality to Get the All Post Comments From Backend to Frontend

---
+++ client/src/components/Comment.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+export default function Comment({ comment }) {
+  const [user, setUser] = useState({});
+
+  console.log(user);
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const res = await fetch(`/api/user/${comment.userId}`);
+        const data = await res.json();
+        if (res.ok) {
+          setUser(data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getUser();
+  }, [comment]);
+
+  return <div></div>;
+}
