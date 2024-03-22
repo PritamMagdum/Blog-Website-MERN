2f3794d13372b3821ae60f37d59d4c8d8e7e3099
Pritam <pritammagdum24@gmail.com>
2024-03-22 11:35:13 +0530

    Added Admin Dashboard Component to Show All the Activity Related to the Website

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import DashProfile from "../components/DashProfile";
 import DashPosts from "../components/DashPosts";
 import DashUsers from "../components/DashUsers";
 import DashComments from "../components/DashComments";
+import DashboardComponent from "../components/DashboardComponent";
 
 export default function Dashboard() {
   const [tab, setTab] = useState("");
@@ -36,6 +37,9 @@ export default function Dashboard() {
 
       {/* All Comments */}
       {tab === "comments" && <DashComments />}
+
+      {/* Admin Dashboard */}
+      {tab === "dash" && <DashboardComponent />}
     </div>
   );
 }
